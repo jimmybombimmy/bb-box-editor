fcf1097477e1fafba25433fcfdbeeb180e22b919
James Peters <jamespeters92@gmail.com>
2026-01-04 20:56:04 +0000

    feat: BB-9: Track boundingClientRect and mousePosition

--- src/components/GridWorkspace/EditableBox/EditableBox.tsx
+++ src/components/GridWorkspace/EditableBox/EditableBox.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
+import useMousePosition from "../../../hooks/useMousePosition"
 
 import "./EditableBox.css"
 
@@ -8,8 +9,23 @@ interface EditableBoxProps {
 
 export function EditableBox(props: EditableBoxProps) {
   const [isDraggable, setIsDraggable] = useState(false)
+  const { x, y } = useMousePosition();  
+
+
+  const box = useRef<HTMLInputElement>(null)
+  let rect = box.current?.getBoundingClientRect()
 
   const {mouseDown} = props
+
+  
+  useEffect(() => {
+    if (box.current) {
+      rect = box.current.getBoundingClientRect();
+      console.log(rect)
+    }
+
+  }, [isDraggable])
+
 
   if (!mouseDown && isDraggable) {
     setIsDraggable(false)
@@ -19,13 +35,20 @@ export function EditableBox(props: EditableBoxProps) {
     setIsDraggable(true)
   }
 
+  function handleMouseOver() {
+    if (isDraggable) console.log("boxcurrent", rect)
+  }
+
   return (
     <>
-      <main id="editable-box" onMouseDown={boxMouseDown}>
+      <main id="editable-box" ref={box} onMouseDown={boxMouseDown} onMouseOver={handleMouseOver}>
         <h1>Box Info:</h1>
         <ul>
           <li>Draggable: {String(isDraggable)}</li>
           <li>Mouse Down: {String(mouseDown)}</li>
+          <li>X Mouse Position: {x} </li>
+          <li>Y Mouse Position: {y} </li>
+
         </ul>
       </main>
     </>
