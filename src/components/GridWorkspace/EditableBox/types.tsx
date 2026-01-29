9f1deab874cd879fd438757d2d4b07ef7ce97a82
James Peters <124522902+jimmybombimmy@users.noreply.github.com>
2026-01-29 20:23:42 +0000

    Revert "Revert "feat: BB-9: Create draggable box""

---
+++ src/components/GridWorkspace/EditableBox/types.tsx
@@ -0,0 +1,20 @@
+export interface EditableBoxProps {
+  mouseDown: boolean,
+  gridRect: DOMRect | null
+}
+
+export interface Position {
+  x: number,
+  y: number
+}
+
+export type AxisLC = "x" | "y"
+export type AxisUC = "X" | "Y"
+
+export interface GridTrapPayload {
+  event: MouseEvent, 
+  mousePos: Position, 
+  rect: DOMRect, 
+  gridRect: DOMRect, 
+  borderWidth: number
+}
