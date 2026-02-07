f266c662a0fe983e4ed4bcfb21c8bb0c66e9e59c
James Peters <jamespeters92@gmail.com>
2026-02-07 11:21:04 +0000

    refactor: BB-00: Minor changes to px position info in box

--- src/components/GridWorkspace/EditableBox/EditableBox.tsx
+++ src/components/GridWorkspace/EditableBox/EditableBox.tsx
@@ -59,21 +59,18 @@ export function EditableBox(props: EditableBoxProps) {
   }
 
   return (
-    <>
-      <main id="editable-box" ref={boxRef} onMouseDown={() => setIsDraggable(true)} style={{left: positionDifference.x, top: positionDifference.y, position: "relative"}}>
-        <h1>Box Info:</h1>
-        <ul unselectable="on">
-          <li>Draggable: {String(isDraggable)}</li>
-          <li>Mouse Down: {String(mouseDown)}</li>
-          <li>X Mouse Position: {mousePos.x} </li>
-          <li>Y Mouse Position: {mousePos.y} </li>
-          <li>X Dragged: {positionDifference.x} </li>
-          <li>Y Dragged: {positionDifference.y} </li>
-          <li>This X: {rect?.x}</li>
-          <li>This Y: {rect?.y}</li>
-          <li>isMouseInBox (and Mouse Down): {String(isMouseInBounds(mousePos))} </li>
-        </ul>
-      </main>
-    </>
+    <main id="editable-box" ref={boxRef} onMouseDown={() => setIsDraggable(true)} style={{left: positionDifference.x, top: positionDifference.y, position: "relative"}}>
+      <h1>Box Info:</h1>
+      <ul unselectable="on">
+        <li>Draggable: {String(isDraggable)} </li>
+        <li>Mouse Down: {String(mouseDown)} </li>
+        <li>X Mouse Position: {Math.round(mousePos.x)}px </li>
+        <li>Y Mouse Position: {Math.round(mousePos.y)}px </li>
+        <li>X Dragged: {Math.round(positionDifference.x)}px </li>
+        <li>Y Dragged: {Math.round(positionDifference.y)}px </li>
+        <li>X Box Window Position: {rect?.x ? Math.round(rect?.x) : 0}px </li>
+        <li>Y Box Window Position: {rect?.x ? Math.round(rect?.y) : 0}px </li>
+      </ul>
+    </main>
   )
 }
